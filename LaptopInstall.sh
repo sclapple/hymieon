0378444d8bafd6aa842847de42974d39fabe7795
sclapple <139606543+sclapple@users.noreply.github.com>
2025-06-23 02:58:24 -0400

    Update LaptopInstall.sh

--- LaptopInstall.sh
+++ LaptopInstall.sh
@@ -11,5 +11,5 @@ sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/ya
        	pipewire-alsa linux-headers xwaylandvideobridge polkit-gnome gnome-disk-utility \
 	timeshift xorg-xhost uwsm fzf fastanime mpv fish fisher wallust \
  	aic94xx-firmware ast-firmware linux-firmware-qlogic linux-firmware-bnx2x linux-firmware-liquidio \
-   	linux-firmware-mellanox linux-firmware-nfp wd719x-firmware upd72020x-fw \
+   	linux-firmware-mellanox linux-firmware-nfp wd719x-firmware upd72020x-fw cava firefox \
        	pamac-all networkmanager lidm && sudo systemctl enable fstrim.timer && sudo systemctl enable NetworkManager.service && sudo systemctl enable lidm.service && sh <(curl -L https://raw.githubusercontent.com/JaKooLit/Arch-Hyprland/main/auto-install.sh)
